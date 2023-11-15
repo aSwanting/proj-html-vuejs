76fe30295463e9302d4686ea4baffc9df865c474
aSwanting <gab.damico@gmail.com>
2023-11-15 14:45:33 +0100

    add: Clean Vite + Vue, Title, Favicon

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>PIZZA TIME</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@use "./styles/general.scss";
+@use "./styles/partials/reset";
+</style>
