924358516dcf5895b7a2fa91543f655223d1d23b
aSwanting <gab.damico@gmail.com>
2023-11-17 21:07:06 +0100

    add: scrollToTop pizza slice

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
   <AppHeader :nav="nav" />
   <AppMain />
   <AppFooter :footer="footerInfo" />
+  <div class="top-slice" @click="scrollToTop()">
+    <img src="./assets/svg/svg-4.svg" />
+  </div>
 </template>
 
 <script>
@@ -20,6 +23,11 @@ export default {
       store,
     };
   },
+  methods: {
+    scrollToTop() {
+      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    },
+  },
   computed: {
     nav() {
       return this.store.nav;
@@ -34,4 +42,19 @@ export default {
 <style lang="scss">
 @use "./styles/general.scss";
 @use "./styles/partials/reset";
+.top-slice {
+  cursor: pointer;
+  position: fixed;
+  bottom: 3%;
+  right: 2%;
+  width: 50px;
+  aspect-ratio: 1;
+  border-radius: 50%;
+  background-color: rgba(255, 255, 255, 0.8);
+  z-index: 9999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.2);
+}
 </style>
